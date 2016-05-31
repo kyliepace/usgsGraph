3f4960063498af555295c63e9451ced40f05476b
Kylie <kyliepace@gmail.com>
2016-05-30 20:34:27 -0700

    problem with results of request

--- js/controller.js
+++ js/controller.js
@@ -1,15 +1,21 @@
 var controllers = function(modelObject, viewObject){
     // 1.getLocation
-    //Q: why do I have to write .prototype?
+    //Q: why do I have to write .prototype if I use constructor function for usgs object?
     usgs.getLocation();
+    //getLocation calls writeRequest and updates usgs.request
     
     // 2.sendRequest
-    usgs.sendRequest(usgs.request);
+    //is usgs.request being sent before the getLocation is complete?
+    if (usgs.request.bBox!== ""){
+        usgs.sendRequest(usgs.request);
+    }
     
-    // 3.populateSeries with library's results as input (done in sendRequest)
+    // 3.populateSeries with library's results as input
+    //this should be in sendRequest .done but I don't think it's working
+    usgs.populateSeries();
     
     // 4.make flowSeries from populated array
-    var newFlow = viewObject.makeFlowSeries(modelObject.xData, modelObject.gageName, modelObject.yData);
+    var newFlow = viewObject.makeFlowSeries(usgs.xData, usgs.gageName, usgs.yData);
     
     // 5.add the new flowSeries to the sites array
     modelObject.addFlowSeries(newFlow);
--- js/usgsLib.js
+++ js/usgsLib.js
@@ -16,16 +16,6 @@ var usgs = {
       yData: []
     }
 };
-
-  //writeRequest turns the getCurrentPosition object into a string in the request
-usgs.writeRequest=function(position){
-    var long=position.coords.longitude.toString().slice(0,11);
-    var lat=position.coords.latitude.toString().slice(0,9);
-    var longExt=(position.coords.longitude+1).toString().slice(0,11);
-    var latExt=(position.coords.latitude+1).toString().slice(0,9);  
-    usgs.request.bBox = long+","+lat+","+longExt+","+latExt;
-};
-
 //getLocation method will need to be called from controller
 usgs.getLocation=function(){
     if (navigator.geolocation) {
@@ -39,6 +29,17 @@ usgs.getLocation=function(){
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
 };
+
+  //writeRequest turns the getCurrentPosition object into a string in the request
+usgs.writeRequest=function(position){
+    var long=position.coords.longitude.toString().slice(0,11);
+    var lat=position.coords.latitude.toString().slice(0,9);
+    var longExt=(position.coords.longitude+1).toString().slice(0,11);
+    var latExt=(position.coords.latitude+1).toString().slice(0,9);  
+    usgs.request.bBox = long+","+lat+","+longExt+","+latExt;
+};
+
+
   //sendRequest sends the request written by writeRequest
   //will need to be called from controller
 usgs.sendRequest=function(request){
@@ -49,17 +50,19 @@ usgs.sendRequest=function(request){
       data: request,
       type: "GET"
     })
-    //no callback if successful; will be handled from controller
+    //populateSeries if complete. 
+    //why is this not working?
     .done(usgs.populateSeries)
     .fail(function(jqXHR, error){
       console.log("error sending request");
-    })
-  
+    })  
 };
 
 //the populateSeries method pushes the usgsData into yData and xData arrays
 usgs.populateSeries = function(results){
   console.log(results);
+  //results are undefined
+  //the results appear to be not json again
     usgs.results.numberOfSites=results.value.timeSeries.length;
     //show the name of the result
     usgs.results.gageName=results.value.timeSeries[0].sourceInfo.siteName;
